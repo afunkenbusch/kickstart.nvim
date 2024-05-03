b29740967317359f24794c94439804e7a4e75a2e
Adam Funkenbusch <adamfunkenbusch@gmail.com>
2024-05-03 11:31:19 -0500

    init.lua: Add vim-tmux-navigator
    This allows navigation between vim and tmux panes using the same
    keybindings.

--- init.lua
+++ init.lua
@@ -226,6 +226,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'christoomey/vim-tmux-navigator',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
